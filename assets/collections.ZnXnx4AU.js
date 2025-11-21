a9218108ff02000579bd687eea8164bf85ef244a
liveou <liveou@126.com>
2025-11-22 00:22:43 +0800

    腾讯cdn测试

--- assets/collections.ZnXnx4AU.js
+++
@@ -1,1 +0,0 @@
-import"./chunks/@vueuse/motion.azCphnVb.js";import{u as l}from"./chunks/vue-router.omV95bea.js";import{A as t}from"./framework.Dh18Cujy.js";const e=[];function a(){return{collections:t(()=>e)}}function p(){const o=l(),n=t(()=>o.path.startsWith("/collections/")?o.path.split("/")[2]:"");return{collection:t(()=>e.find(c=>c.key===n.value))}}export{a,p as u};
