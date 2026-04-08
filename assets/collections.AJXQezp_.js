18b399432530185e60a45ec7f98452b66d75d890
Hebaiju <wbix@qq.com>
2026-04-08 19:14:15 +0800

    添加评论系统
    成功添加评论系统

---
+++ assets/collections.AJXQezp_.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.Bt4Sh_SY.js";import{u as l}from"./chunks/vue-router.F9x_qN8-.js";import{A as t}from"./framework.fTMJ0I03.js";const e=[];function a(){return{collections:t(()=>e)}}function p(){const o=l(),n=t(()=>o.path.startsWith("/collections/")?o.path.split("/")[2]:"");return{collection:t(()=>e.find(c=>c.key===n.value))}}export{a,p as u};
---
+++ assets/empty.C3OJwSgh.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.Bt4Sh_SY.js";import{a8 as o,a9 as t,aa as r,ab as a}from"./framework.fTMJ0I03.js";const c={};function n(s,_){const e=t("RouterView");return a(),r(e)}const f=o(c,[["render",n]]);export{f as default};
