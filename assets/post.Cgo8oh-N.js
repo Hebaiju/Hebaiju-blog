5a5ac6c000aa0995c482d5f96a9f058e559a61b7
Hebaiju <wbix@qq.com>
2026-04-08 19:29:46 +0800

    更新文章

---
+++ assets/post.Cgo8oh-N.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.Bt4Sh_SY.js";import{o as t}from"./app.h4q5pLSW.js";import{A as e}from"./framework.fTMJ0I03.js";function m(o){if(!o)return{color:"",icon:"",styles:void 0};const r=t();o in r.value.types||(o="link");const s=r.value.types[o].color,i=r.value.types[o].icon,n=e(()=>o?{"--card-c-primary":s}:void 0);return{color:s,icon:i,styles:n}}export{m as u};
